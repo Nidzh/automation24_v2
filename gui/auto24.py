fd6801f910161ade5dc702db14e325889f602955
Nidzhat Ragimov <n.ragimov@outlook.com>
2022-11-26 02:50:21 +0400

    add

--- gui/auto24.py
+++ gui/auto24.py
@@ -1,12 +1,12 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from pathlib import Path
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(516, 304)
+        MainWindow.resize(400, 100)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("../DE.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap('de.png'), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
@@ -20,11 +20,12 @@ class Ui_MainWindow(object):
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
 
-        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setProperty("value", 50)
-        self.progressBar.setTextDirection(QtWidgets.QProgressBar.TopToBottom)
-        self.progressBar.setObjectName("progressBar")
-        self.verticalLayout.addWidget(self.progressBar)
+
+
+        self.pushButton2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton2.setObjectName("pushButton2")
+        self.verticalLayout.addWidget(self.pushButton2)
+        MainWindow.setCentralWidget(self.centralwidget)
 
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setObjectName("pushButton")
@@ -37,4 +38,5 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Automation24"))
-        self.pushButton.setText(_translate("MainWindow", "Запустить браузер"))
+        self.pushButton.setText(_translate("MainWindow", "Запустить парсинг"))
+        self.pushButton2.setText(_translate("MainWindow", "Определить время парсинга."))
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
+import time
 from pathlib import Path
+
 from PyQt5 import QtWidgets
-from selenium_24.parser import Auto24Parser
 from gui import auto24
+from selenium_24.parser import Auto24Parser
 
 # Path
 file_path = Path.cwd() / 'files'
@@ -14,29 +16,47 @@ class ExampleApp(QtWidgets.QMainWindow, auto24.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.progressBar.setProperty('value', 0)
         self.pushButton.clicked.connect(self.run_parser)
+        self.pushButton2.clicked.connect(self.check_time)
 
         self.input_file = file_path / 'article.xlsx'
         self.output_file = file_path / 'result.xlsx'
+
+        self.label.setText('Для работы парсера необходимо разместить файл с названием article.xlsx в папке files.\n'
+                           'В эту же папку будет записан файл result.xlsx с результатами парсинга.\n'
+                           'Страна: GERMANY. Тип: BTC')
 
     def run_parser(self):
         parser = Auto24Parser(
             input_fl=self.input_file,
             output_fl=self.output_file
         )
+        try:
+            parser.run_parser()
+            time.sleep(3)
+            self.close()
+        except UserWarning:
+            self.label.setText(f'Файл article.xlsx в папке /files не найден.')
 
-        parser.run_parser()
 
-        # self.label.setText(f'Файл загружен.\nКол-во артикулей: {length}. \n'
-        #                    f'Примерное время обработки: {parsing_time}')
 
+    def check_time(self):
+        parser = Auto24Parser(
+            input_fl=self.input_file,
+            output_fl=self.output_file
+        )
+
+        try:
+            lenght, time = parser.get_parsing_time()
+            self.label.setText(f'Кол-во Артикулей: {lenght}\nПримерное время работы: {time}')
+        except:
+            self.label.setText(f'Файл article.xlsx в папке /files не найден.')
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
-    window = ExampleApp()  # Создаём объект класса ExampleApp
-    window.show()  # Показываем окно
-    app.exec_()  # и запускаем приложение
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    app.exec_()
 
 
 if __name__ == '__main__':
